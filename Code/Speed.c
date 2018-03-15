56b2c8d70367118b15096282904b8b99ffabc553
Ro-Bert-Web <37278841+Ro-Bert-Web@users.noreply.github.com>
2018-03-15 10:31:03 -0700

    Update Speed.c

--- Code/Speed.c
+++ Code/Speed.c
@@ -1,9 +1,7 @@
 #pragma config(I2C_Usage, I2C1, i2cSensors)
 #pragma config(Sensor, dgtl1,  sensorPort,     sensorQuadEncoder)
 #pragma config(Sensor, I2C_1,  ,               sensorQuadEncoderOnI2CPort,    , AutoAssign )
-#pragma config(Sensor, I2C_2,  ,               sensorQuadEncoderOnI2CPort,    , AutoAssign )
 #pragma config(Motor,  port2,           encoderPort,   tmotorVex393_MC29, openLoop, encoderPort, I2C_1)
-#pragma config(Motor,  port3,           rawEncoderPort, tmotorVex393_MC29, openLoop, encoderPort, I2C_2)
 
 ///////////////////////////////////////
 //-|-------------------------------|-//
@@ -15,7 +13,7 @@
 #include "Competition Language.c"
 
 task main(){
-	int sensor, encoder, rawEncoder;
+	int sensor, encoder;
 
 	//Set Speed Accuracy (Not Necessary, 33 Is Default)
 	setSpeedAccuracy(33);
@@ -27,8 +25,5 @@ task main(){
 
 		//Get The Speed Of The Encoder
 		encoderSpeed(encoder, encoderPort);
-
-		//Get The Speed Of The Raw Encoder
-		rawEncoderSpeed(rawEncoder, rawEncoderPort);
 	}
 }
